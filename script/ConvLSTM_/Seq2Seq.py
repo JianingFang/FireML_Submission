e437922f795119fb2e404980992b13962791b434
Jianing Fang <jf3423@g101.cm.cluster>
2021-11-27 21:15:13 -0500

    updated fireml to accomodate entire sequence for validation

--- script/ConvLSTM_/Seq2Seq.py
+++ script/ConvLSTM_/Seq2Seq.py
@@ -5,7 +5,7 @@ from ConvLSTM import ConvLSTM
 class Seq2Seq(nn.Module):
 
     def __init__(self, num_channels, num_kernels, kernel_size, padding, 
-    activation, frame_size, num_layers):
+    activation, frame_size, num_layers, return_sequences):
 
         super(Seq2Seq, self).__init__()
 
@@ -22,6 +22,8 @@ class Seq2Seq(nn.Module):
         self.sequential.add_module(
             "batchnorm1", nn.BatchNorm3d(num_features=num_kernels)
         ) 
+        
+        self.return_sequences=return_sequences
 
         # Add rest of the layers
         for l in range(2, num_layers+1):
@@ -41,14 +43,21 @@ class Seq2Seq(nn.Module):
         self.conv = nn.Conv2d(
             in_channels=num_kernels, out_channels=num_channels,
             kernel_size=kernel_size, padding=padding)
+        
+        self.conv_sequence = nn.Conv3d(
+            in_channels=num_kernels, out_channels=num_channels,
+            kernel_size=tuple([kernel_size[0]]*3), padding=tuple([padding[0]]*3))
 
     def forward(self, X):
 
         # Forward propagation through all the layers
         output = self.sequential(X)
-
-        # Return only the last output frame
-        output = self.conv(output[:,:,-1])
+        
+        if self.return_sequences:
+            output=self.conv_sequence(output)
+        else:
+            # Return only the last output frame
+            output = self.conv(output[:,:,-1])
         
         return nn.Sigmoid()(output)
 
